6037c036cda2d0b661242f468ab05a74711d2575
KingGyeongHoo <fmaxgo@naver.com>
2023-04-07 17:10:10 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import logo from './codestates-logo.png';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Discussions from './components/discussions';
+import Modal from './components/modal';
+import AddDiscussion from './components/adddiscussion';
+
+const Header = function () {
+  return (
+    <header>
+      <div className="img_container">
+        <a href="https://www.codestates.com/"><img src={logo} id="codestates_logo" /></a>
+      </div>
+      <div className="menu_container">
+        <a href="https://urclass.codestates.com/">유어클래스</a>
+        <a href="https://urclass.codestates.com/mypage/">마이페이지</a>
+      </div>
+    </header>
+  )
+}
+
 
 function App() {
+  const [isModalOn, setModal] = useState(false)
+  const [discussionData, setData] = useState([])
+  const [isEntried, serIsEntrieds] = useState(false)
+
+  const modalHandler = function (modalState) {
+    setModal(modalState)
+  }
+  useEffect(() => {
+    fetch("http://localhost:4000/discussions/")
+      .then(response => response.json())
+      .then(json => {
+        setData(json)
+      })
+  }, [])
+
+  const addDiscussion = function (newDiscussion) {
+    setData([newDiscussion, ...discussionData])
+  }
+
+  const discussionsController = function(bool){
+    serIsEntrieds(bool)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Header />
+      <AddDiscussion handler={modalHandler} />
+      {(isModalOn) ? <Modal handler={modalHandler} discussionHandler={addDiscussion} entryHandler={discussionsController}/> : null}
+      <Discussions discussion={discussionData}/>
+    </main>
+  )
 }
 
 export default App;
